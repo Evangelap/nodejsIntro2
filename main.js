0cd736c17482b8e7d64cd6d5aa2ef4d83b55269f
Evangelap <evapapageorgiou@hotmail.com>
2022-01-26 18:33:16 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// npm: node package manager
-// npm init -> δημιουργεί ένα αρχείο package.json
-// npm init -y απαντάει σε όλα ναι
-// package name: το όνομα όλου του πακέτου
-// version: θα μπορούσα να γράψω 0.0.1
-// description: Just to showcase nodejs packages
-// entry point: από ποιο αρχείο ξεκινάμε το main
-// test command:
-// git repository: https://github.com/Evangelap/nodejsIntro  <- η διεύθυνση από ένα repository, προσέχω να μην έχει κατάληξη .git η διεύθυνση
-// keywords: tags σαν το φβ
-// author: καλό θα ήταν να βάλω
 
 console.log("Hello World");
 console.log("Hello World");
